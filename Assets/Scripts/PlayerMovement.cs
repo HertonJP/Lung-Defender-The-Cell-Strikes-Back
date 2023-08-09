0120d523d8979b0216fe1f168c0eda19af259de2
HertonJP <125948356+HertonJP@users.noreply.github.com>
2023-08-09 09:53:39 +0700

    animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,39 +7,65 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] public float movementSpeed = 5f;
     public Rigidbody2D rb;
     public Camera cam;
-    private bool facingRight = true;
+    private bool facingRight = false;
+    private Animator animator;
+    private bool isRolling = false;
+    private Vector3 rollDirection;
 
     Vector2 movement;
     Vector2 mousePos;
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        if ((movement.x < 0 && facingRight) || (movement.x > 0 && !facingRight))
+        
+
+        if (Input.GetKeyDown(KeyCode.Q) && !isRolling)
         {
-            Flip();
-        }
-        if (transform.position.y >= 39)
-        {
-            transform.position = new Vector3(transform.position.x, 39, 0);
+            StartRoll();
         }
 
-        if (transform.position.y <= -39)
-        {
-            transform.position = new Vector3(transform.position.x, -39, 0);
-        }
-        if (transform.position.x >= 52)
-        {
-            transform.position = new Vector3(52, transform.position.y, 0);
-        }
-        if (transform.position.x <= -52)
-        {
-            transform.position = new Vector3(-52, transform.position.y, 0);
-        }
-
-
+        bool isMoving = movement.magnitude > 0.1f;
+        animator.SetBool("isRunning", isMoving);
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void StartRoll()
+    {
+        animator.SetTrigger("isRolling");
+
+        rollDirection = transform.forward;
+
+        isRolling = true;
+        Vector3 targetPosition = transform.position + rollDirection * 2f;
+        StartCoroutine(RollCoroutine(targetPosition));
+    }
+
+    private IEnumerator RollCoroutine(Vector3 targetPosition)
+    {
+        float elapsedTime = 0f;
+        float rollDuration = 0.5f;
+
+        while (elapsedTime < rollDuration)
+        {
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, elapsedTime / rollDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+        isRolling = false;
+    }
+
+    public void FinishRoll()
+    {
+        isRolling = false;
     }
 
     private void FixedUpdate()
