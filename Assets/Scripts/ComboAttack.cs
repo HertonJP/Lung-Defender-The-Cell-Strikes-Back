844fc120f1cb240c7fc96daf53163cb13768049d
jebbermeister <jefferdev123@gmail.com>
2023-08-09 17:30:53 +0700

    animation fixed

--- Assets/Scripts/ComboAttack.cs
+++ Assets/Scripts/ComboAttack.cs
@@ -4,8 +4,9 @@ using System;
 
 public class ComboAttack : MonoBehaviour
 {
+    public int comboCount;
     private Animator animator;
-    private bool isAttacking = false;
+    private bool isAttacking;
     private float lastAttackTime = 0f;
     private float comboTimeout = 1f;
 
@@ -14,38 +15,38 @@ public class ComboAttack : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
-    private void Update()
-    {
-        // Check for left-click input
-        if (Input.GetButtonDown("Fire1"))
-        {
-            if (!isAttacking)
-            {
-                // Start the combo
-                StartCoroutine(ComboTimer());
-                lastAttackTime = Time.time;
-                isAttacking = true;
-                PlayAttackAnimation(1);
-            }
-            else
-            {
-                // Check if the player can continue the combo
-                float timeSinceLastAttack = Time.time - lastAttackTime;
-                if (timeSinceLastAttack <= comboTimeout)
-                {
-                    lastAttackTime = Time.time;
-                    int nextAttackAnim = (GetCurrentAttackAnimation() % 3) + 1;
-                    PlayAttackAnimation(nextAttackAnim);
-                }
-                else
-                {
-                    // Combo timeout, restart the combo
-                    lastAttackTime = Time.time;
-                    PlayAttackAnimation(1);
-                }
-            }
-        }
-    }
+    //private void Update()
+    //{
+    //    // Check for left-click input
+    //    if (Input.GetButtonDown("Fire1"))
+    //    {
+    //        if (!isAttacking)
+    //        {
+    //            // Start the combo
+    //            StartCoroutine(ComboTimer());
+    //            lastAttackTime = Time.time;
+    //            isAttacking = true;
+    //            PlayAttackAnimation(1);
+    //        }
+    //        else
+    //        {
+    //            // Check if the player can continue the combo
+    //            float timeSinceLastAttack = Time.time - lastAttackTime;
+    //            if (timeSinceLastAttack <= comboTimeout)
+    //            {
+    //                lastAttackTime = Time.time;
+    //                int nextAttackAnim = (GetCurrentAttackAnimation() % 3) + 1;
+    //                PlayAttackAnimation(nextAttackAnim);
+    //            }
+    //            else
+    //            {
+    //                // Combo timeout, restart the combo
+    //                lastAttackTime = Time.time;
+    //                PlayAttackAnimation(1);
+    //            }
+    //        }
+    //    }
+    //}
 
     private void PlayAttackAnimation(int comboStep)
     {
@@ -62,5 +63,32 @@ public class ComboAttack : MonoBehaviour
     {
         yield return new WaitForSeconds(comboTimeout);
         isAttacking = false;
+    }
+
+    public void StartCombo()
+    {
+        isAttacking = false;
+        if(comboCount < 3)
+        {
+            comboCount++;
+        }
+    }
+    public void PlayCombo()
+    {
+        if (Input.GetButtonDown("Fire1") && !isAttacking)
+        {
+            isAttacking = true;
+            animator.SetTrigger("" + comboCount);
+        }
+    }
+    public void Update()
+    {
+        PlayCombo();
+    }
+
+    public void StopCombo()
+    {
+        isAttacking = false;
+        comboCount = 0;
     }
 }
