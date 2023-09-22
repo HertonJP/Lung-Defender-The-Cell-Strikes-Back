1af3ec84e8c2b1013b9fbbed0aab6ad38c13eb2b
HertonJP <125948356+HertonJP@users.noreply.github.com>
2023-09-22 23:10:50 +0700

    finishing

--- Assets/Scripts/LabClick.cs
+++ Assets/Scripts/LabClick.cs
@@ -10,7 +10,7 @@ public class LabClick : MonoBehaviour
     {
         if (Time.time > nextActionTime)
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0) && Time.timeScale != 0)
             {
                 Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
                 RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
