8eb3c63e1cd0a2b91dcd87cc190b74cc03f365c8
HertonJP <125948356+HertonJP@users.noreply.github.com>
2023-10-17 00:55:44 +0700

    attack bugfix

--- Assets/Scripts/ComboAttack.cs
+++ Assets/Scripts/ComboAttack.cs
@@ -7,63 +7,22 @@ public class ComboAttack : MonoBehaviour
     public int comboCount;
     private Animator animator;
     private bool isAttacking;
-    private float lastAttackTime = 0f;
     private float comboTimeout = 1f;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
     }
-
-    //private void Update()
-    //{
-    //    // Check for left-click input
-    //    if (Input.GetButtonDown("Fire1"))
-    //    {
-    //        if (!isAttacking)
-    //        {
-    //            // Start the combo
-    //            StartCoroutine(ComboTimer());
-    //            lastAttackTime = Time.time;
-    //            isAttacking = true;
-    //            PlayAttackAnimation(1);
-    //        }
-    //        else
-    //        {
-    //            // Check if the player can continue the combo
-    //            float timeSinceLastAttack = Time.time - lastAttackTime;
-    //            if (timeSinceLastAttack <= comboTimeout)
-    //            {
-    //                lastAttackTime = Time.time;
-    //                int nextAttackAnim = (GetCurrentAttackAnimation() % 3) + 1;
-    //                PlayAttackAnimation(nextAttackAnim);
-    //            }
-    //            else
-    //            {
-    //                // Combo timeout, restart the combo
-    //                lastAttackTime = Time.time;
-    //                PlayAttackAnimation(1);
-    //            }
-    //        }
-    //    }
-    //}
-
-    private void PlayAttackAnimation(int comboStep)
+    public void Update()
     {
-        animator.SetInteger("ComboStep", comboStep);
-        animator.SetTrigger("Attack");
+        if (Input.GetButtonDown("Fire1") && !isAttacking && Time.timeScale != 0)
+        {
+            Debug.Log("dalem input");
+            isAttacking = true;
+            animator.SetTrigger("" + comboCount);
+        }
     }
 
-    private int GetCurrentAttackAnimation()
-    {
-        return animator.GetInteger("ComboStep");
-    }
-
-    private IEnumerator ComboTimer()
-    {
-        yield return new WaitForSeconds(comboTimeout);
-        isAttacking = false;
-    }
 
     public void StartCombo()
     {
@@ -73,18 +32,7 @@ public class ComboAttack : MonoBehaviour
             comboCount++;
         }
     }
-    public void PlayCombo()
-    {
-        if (Input.GetButtonDown("Fire1") && !isAttacking && Time.timeScale != 0)
-        {
-            isAttacking = true;
-            animator.SetTrigger("" + comboCount);
-        }
-    }
-    public void Update()
-    {
-        PlayCombo();
-    }
+    
 
     public void StopCombo()
     {
