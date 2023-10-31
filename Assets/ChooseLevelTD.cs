588e7e08e08cdc9cb71eb0143945ed553ef7e4cd
makuroo <richardhuang1705@gmail.com>
2023-10-31 10:53:22 +0700

    push

--- Assets/ChooseLevelTD.cs
+++ Assets/ChooseLevelTD.cs
@@ -9,19 +9,6 @@ public class ChooseLevelTD: MonoBehaviour
     [SerializeField] List<Button> levelButton = new();
     private void OnEnable()
     {
-        //if (PlayerPrefs.GetInt(GameManager.Instance.lastTDLevelPlayerPrefs) == 0)
-        //    levelButton[0].interactable = true;
-        //else if (PlayerPrefs.GetInt(GameManager.Instance.lastTDLevelPlayerPrefs) == 1)
-        //{
-        //    levelButton[0].interactable = true;
-        //    levelButton[1].interactable = true;
-        //}
-        //else if (PlayerPrefs.GetInt(GameManager.Instance.lastTDLevelPlayerPrefs) == 2)
-        //{
-        //    levelButton[0].interactable = true;
-        //    levelButton[1].interactable = true;
-        //    levelButton[2].interactable = true;
-        //}
         for (int i = 0; i < PlayerPrefs.GetInt(GameManager.Instance.lastTDLevelPlayerPrefs,0) + 1; i++)
         {
             levelButton[i].interactable = true;
